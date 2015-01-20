e30f739f77e2baeeb4fe40944fd55a3aeac714ec
m23project <hhabermann@pc-kiel.de>
2015-01-20 12:03:22 +0100

    a

---
+++ polls/single.php
@@ -0,0 +1,11 @@
+<?php
+
+$i = 0;
+
+$poll['type'] = 'single';
+$poll['q'] = 'What movie do we want to see?';
+$poll['a'][$i++] = 'Movie 1';
+$poll['a'][$i++] = 'Movie 2';
+$poll['a'][$i++] = 'Movie 3';
+
+?>
