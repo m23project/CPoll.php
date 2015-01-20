8ff243d2daeddaea5fe74c72c4c5e4da9fa83307
m23project <hhabermann@pc-kiel.de>
2015-01-20 13:00:21 +0100

    a

---
+++ polls/multi.php
@@ -0,0 +1,11 @@
+<?php
+
+$i = 0;
+
+$poll['type'] = 'multi';
+$poll['q'] = 'What are your most loved foods?';
+$poll['a'][$i++] = 'Food 1';
+$poll['a'][$i++] = 'Food 2';
+$poll['a'][$i++] = 'Food 3';
+
+?>
